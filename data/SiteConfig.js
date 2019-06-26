f61c6cec0135f390bd27653ee2d9f8e51608bb2f
ryotogashi <ryo.togashi.ca@gmail.com>
2019-06-26 14:07:58 -0700

    edit siteconfig

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,42 +1,49 @@
 const config = {
-  siteTitle: "Gatsby Material Starter", // Site title.
-  siteTitleShort: "GM Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "GatsbyJS Material Starter", // Alternative site title for SEO.
-  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
-  fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
-  siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
-  disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", // Disqus shortname.
-  postDefaultCategoryID: "Tech", // Default category for posts.
-  dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
-  dateFormat: "DD/MM/YYYY", // Date format for display.
-  userName: "Material User", // Username to display in the author segment.
-  userEmail: "MaterialUser@example.com", // Email used for RSS feed's author segment
-  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
-  userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  siteTitle: "Ryo Togashi",
+  siteTitleShort: "ryotogashi",
+  siteUrl: "https://vagr9k.github.io",
+  pathPrefix: "/gatsby-material-starter", //todo Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  fixedFooter: false, //todo Whether the footer component is fixed, i.e. always visible
+  siteDescription: "Ryo Togashi portfolio site.",
+  siteRss: "/rss.xml",
+  siteGATrackingID: "UA-47311644-4", //todo Tracking code ID for google analytics.
+  disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", //todo Disqus shortname.
+  postDefaultCategoryID: "Tech", //todo Default category for posts.
+  dateFromFormat: "YYYY-MM-DD", //todo Date format used in the frontmatter.
+  dateFormat: "DD/MM/YYYY", //todo Date format for display.
+  userName: "RyoTogashi", //todo Username to display in the author segment.
+  userEmail: "ryotogashi.ca@gmail.com", //todo Email used for RSS feed's author segment
+  userTwitter: "", //todo Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Vancouver, BC, Canada", // User location to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", //todo User avatar to display in the author segment.
+  userDescription: "",
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
-      label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-material-starter",
-      iconClassName: "fa fa-github"
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/in/ryotogashi",
+      iconClassName: "fa fa-linkedin-square"
     },
     {
-      label: "Twitter",
-      url: "https://twitter.com/Vagr9K",
-      iconClassName: "fa fa-twitter"
+      label: "GitHub",
+      url: "https://github.com/ryotogashi",
+      iconClassName: "fa fa-github-square"
+    },
+    {
+      label: "Facebook",
+      url: "https://www.facebook.com/ryotogashi304",
+      iconClassName: "fa fa-facebook-square"
     },
     {
       label: "Email",
-      url: "mailto:vagr9k@gmail.com",
+      url: "mailto:ryo.togashi.ca@gmail.com",
       iconClassName: "fa fa-envelope"
+    },
+    {
+      label: "Resume",
+      url:
+        "https://drive.google.com/file/d/1_PvAc43SeKEHPmmEPw-qlDFBtcMRu1HA/view",
+      iconClassName: "fa fa-paperclip"
     }
   ],
   copyright: "Copyright © 2019. Material User" // Copyright string for the footer of the website and RSS feed.
