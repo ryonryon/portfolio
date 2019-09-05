c8123361142fa41436e5fbf0f1df783ddf9c0a2e
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-05 09:46:18 -0300

    change logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "components/Router";
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import Logo from "../../assets/logo.png";
 import "./header.scss";
 
 const Header = () => (
   <nav>
     <div className="header box-shadow">
       <Link className="logo-container" to="/">
-        {/* <Logo className="logo" /> */}Home
+        <img src={Logo} alt="logo image" />
       </Link>
       <div className="options">
-        <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
         <Link to="/dynamic">Dynamic</Link>
