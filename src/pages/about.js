bf47d54df7d7b973efd1d21941e6b80c8890012d
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-12 09:02:39 -0300

    add skill section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+
+import avatarImage from "../assets/avatar.png";
+
+import "./about.scss";
 
 export default () => (
   <div>
-    <p>React Static is a progressive static site generator for React.</p>
+    <div className="about-page">
+      <h2>WHOAMI</h2>
+      <div className="introduction-section">
+        <div className="image-box">
+          <img className="avatar-image" src={avatarImage} alt="avatar" />
+        </div>
+
+        <div className="text-box">
+          <p>
+            Borned and raised in a small town with big-city work ethics, I am a
+            self-taught software engineer, who is mature beyond my years, with
+            six years of work experience. Having an awesome mentor plus
+            self-employed experiences, the contractor sends me to Hokkaido as a
+            representative for the project. My experience working with
+            multinational helped shape my logical thinking.
+          </p>
+        </div>
+      </div>
+      <h2>SKILL</h2>
+      <div className="skill-card card-shadow">
+        <p>
+          Skill skill Skill skill Skill skill Skill skill Skill skill Skill
+          skill Skill skill Skill skill Skill skill Skill skill Skill skill
+          Skill skill Skill skill Skill skill Skill skill Skill skill Skill
+          skill Skill skill Skill skill Skill skill Skill skill Skill skill
+          Skill skill Skill skill Skill skill Skill skill Skill skill Skill
+          skill Skill skill Skill skill Skill skill Skill skill Skill skill
+        </p>
+      </div>
+    </div>
   </div>
-)
+);
