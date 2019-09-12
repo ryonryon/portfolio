c1f4bcd23f62e5b0aeb82970e04f257c83dee9b2
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-12 10:37:59 -0300

    add dart flutter

---
+++ src/assets/tech-logo/tech-logo.js
@@ -0,0 +1,5 @@
+import javascriptLogo from "../assets/tech-logo/javascript.png";
+import pythonLogo from "../assets/tech-logo/python.png";
+
+// export const javascriptLogo;
+// export const pythonLogo;
