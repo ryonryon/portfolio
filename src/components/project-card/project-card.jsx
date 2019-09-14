e667627ea6fdf7753069e4b910680f4571744a0e
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-14 10:23:46 -0700

    add click event

--- src/components/project-card/project-card.jsx
+++ src/components/project-card/project-card.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import "./project-card.scss";
 
-const ProjectCard = () => <div className="project-card box-shadow">card</div>;
+const ProjectCard = () => (
+  <div
+    className="project-card box-shadow"
+    onClick={() => console.log("clicked")}
+  >
+    card
+  </div>
+);
 
 export default ProjectCard;
