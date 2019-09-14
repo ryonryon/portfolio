13d6338da00d51bc0bff05067f46b848e94ec011
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-14 14:35:21 -0700

    add another picture to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 import avatarImage from "../assets/avatar.jpg";
+import avatar1Image from "../assets/avatar1.jpg";
 
 import "./about.scss";
 
 export default () => (
   <div className="about-page">
     <h2>WHOAMI</h2>
+    <div className="introduction-section">
+      <div className="text-box">
+        <p>
+          Lorem ipsum dolor sit amet, vehicula a. Ultricies nec, velit tellus.
+          Lectus id, in consequat, ridiculus porttitor vel. Massa sit, quam non
+          wisi. Quis pellentesque lacus, quisque magna, vestibulum amet. Odio
+          lectus accumsan, augue elementum et.
+        </p>
+      </div>
+      <div className="image-box1">
+        <img className="avatar-image" src={avatar1Image} alt="avatar" />
+      </div>
+    </div>
     <div className="introduction-section">
       <div className="image-box">
         <img className="avatar-image" src={avatarImage} alt="avatar" />
