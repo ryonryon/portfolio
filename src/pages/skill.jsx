359ba85b80a5aebe92dd65a10f724dfb06b73b56
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-17 23:03:46 -0700

    change dir and config
    Signed-off-by: ryotogashi <ryo.togashi.ca@gmail.com>

--- src/pages/skill.jsx
+++ src/pages/skill.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
-import javascriptLogo from "../../assets/tech-logo/javascript.png";
-import typescriptLogo from "../../assets/tech-logo/typescript.png"; // TODO
-import html5Logo from "../../assets/tech-logo/html-5.png";
-import css3Logo from "../../assets/tech-logo/css-3.png";
-import bootstrapLogo from "../../assets/tech-logo/bootstrap.png";
-import csharpLogo from "../../assets/tech-logo/c-sharp.png";
-import javaLogo from "../../assets/tech-logo/java.png";
-import firebaseLogo from "../../assets/tech-logo/firebase.png";
-import githubLogo from "../../assets/tech-logo/github.png";
-import slackLogo from "../../assets/tech-logo/slack.png";
-import sketchLogo from "../../assets/tech-logo/sketch.png";
-import reactLogo from "../../assets/tech-logo/react.png";
-import nodejsLogo from "../../assets/tech-logo/nodejs.png";
-import dartLogo from "../../assets/tech-logo/dart.png";
-import flutterLogo from "../../assets/tech-logo/flutter.png";
-import pythonLogo from "../../assets/tech-logo/python.png";
-import swiftLogo from "../../assets/tech-logo/swift.png";
-import sqlLogo from "../../assets/tech-logo/sql.png";
-import babelLogo from "../../assets/tech-logo/babel.png";
-import webpackLogo from "../../assets/tech-logo/webpack.png";
+import javascriptLogo from "../assets/tech-logo/javascript.png";
+import typescriptLogo from "../assets/tech-logo/typescript.png"; // TODO
+import html5Logo from "../assets/tech-logo/html-5.png";
+import css3Logo from "../assets/tech-logo/css-3.png";
+import bootstrapLogo from "../assets/tech-logo/bootstrap.png";
+import csharpLogo from "../assets/tech-logo/c-sharp.png";
+import javaLogo from "../assets/tech-logo/java.png";
+import firebaseLogo from "../assets/tech-logo/firebase.png";
+import githubLogo from "../assets/tech-logo/github.png";
+import slackLogo from "../assets/tech-logo/slack.png";
+import sketchLogo from "../assets/tech-logo/sketch.png";
+import reactLogo from "../assets/tech-logo/react.png";
+import nodejsLogo from "../assets/tech-logo/nodejs.png";
+import dartLogo from "../assets/tech-logo/dart.png";
+import flutterLogo from "../assets/tech-logo/flutter.png";
+import pythonLogo from "../assets/tech-logo/python.png";
+import swiftLogo from "../assets/tech-logo/swift.png";
+import sqlLogo from "../assets/tech-logo/sql.png";
+import babelLogo from "../assets/tech-logo/babel.png";
+import webpackLogo from "../assets/tech-logo/webpack.png";
 
-import SkillSet from "../../components/skill-set/skill-set";
-import Header from "../../components/header/header";
-import ContactMe from "../../components/contact-me/contact-me";
-import Footer from "../../components/footer/footer";
+import SkillSet from "../components/skill-set/skill-set";
+import Header from "../components/header/header";
+import ContactMe from "../components/contact-me/contact-me";
+import Footer from "../components/footer/footer";
 
 import "./skill.scss";
 
