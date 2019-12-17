99850e2df8268135cfbd01f4ba7059ddc2088679
ryotogashi <ryo.togashi.ca@gmail.com>
2019-12-16 16:01:21 -0800

    add model

---
+++ src/models/Contact.js
@@ -0,0 +1,9 @@
+export default class Contact {
+  constructor(email, facebook, github, linkedIn, resume) {
+    this.email = email;
+    this.facebook = facebook;
+    this.github = github;
+    this.linkedIn = linkedIn;
+    this.resume = resume;
+  }
+}
