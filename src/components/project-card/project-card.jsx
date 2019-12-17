f2e31d7429b188e0a056d3a6c2cea9fd113e3ac0
ryotogashi <ryo.togashi.ca@gmail.com>
2019-12-16 16:43:49 -0800

    made project-card component to functional base component

--- src/components/project-card/project-card.jsx
+++ src/components/project-card/project-card.jsx
@@ -1,30 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./project-card.scss";
 
-class ProjectCard extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="project-card">
-        <a className="button-a" href={`/project/${this.props.projectInfo.id}`}>
-          <figure>
-            <img
-              src={this.props.projectInfo.windowImage}
-              alt={`project image${this.props.projectInfo.id}`}
-            />
-            <figcaption>
-              <h3>{this.props.projectInfo.name}</h3>
-              <p>{`${this.props.projectInfo.technologyUsed.join(" / ")}`}</p>
-            </figcaption>
-          </figure>
-        </a>
-      </div>
-    );
-  }
-}
+const ProjectCard = props => {
+  const { projectInfo } = props;
+  return (
+    <div className="project-card">
+      <a className="button-a" href={`/project/${projectInfo.id}`}>
+        <figure>
+          <img
+            src={projectInfo.windowImage}
+            alt={`project image${projectInfo.id}`}
+          />
+          <figcaption>
+            <h3>{projectInfo.name}</h3>
+            <p>{`${projectInfo.technologyUsed.join(" / ")}`}</p>
+          </figcaption>
+        </figure>
+      </a>
+    </div>
+  );
+};
 
 export default ProjectCard;
